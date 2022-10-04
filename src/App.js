e9328209287a9e467893307e125ae692ee3cc348
NostromosTeaCup <lvanmierlo@protonmail.com>
2022-10-04 13:50:03 +0200

    added buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Button from './components/Button.js';
 import './App.css';
 
 function App() {
   return (
+      <>
       <h1>Handbags & Purses</h1>
+      <nav>
+          <Button isDisabled={false} clickHandler={() => console.log("To the collection!")}>
+              To the collection
+          </Button>
+          <Button isDisabled={false} clickHandler={() => console.log("Shop all bags!")}>
+              Shop all bags
+          </Button>
+          <Button isDisabled={true} clickHandler={() => console.log("Pre-orders")}>
+              pre-orders
+          </Button>
+      </nav>
+      </>
   );
 }
 
