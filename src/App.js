f04ca21ac3dfc3a65e0e5906dea7a94e2a058f72
NostromosTeaCup <lvanmierlo@protonmail.com>
2022-10-04 14:08:44 +0200

    added product component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Button from './components/Button.js';
+import Product from './components/Product';
+import bag_one from './assets/bag_1.png';
+import bag_two from './assets/bag_2.png';
+import bag_three from './assets/bag_3.png';
+import bag_four from './assets/bag_4.png';
 import './App.css';
 
 function App() {
@@ -18,16 +23,32 @@ function App() {
           </Button>
       </nav>
           <main>
-              <article>
-                  <span>
-                  </span>
-                  <p>
-                  </p>
-                  <h4>
-
-                  </h4>
-              </article>
+              <Product
+                  label="Best seller"
+                  img={bag_one}
+                  title="The handy bag"
+                  price={400}
+                  />
+              <Product
+                  label="Best seller"
+                  img={bag_two}
+                  title="The stylish bag"
+                  price={250}
+                  />
+              <Product
+                  label="New collection"
+                  img={bag_three}
+                  title="The simple bag"
+                  price={300}
+              />
+              <Product
+                  label="New collection"
+                  img={bag_four}
+                  title="The trendy bag"
+                  price={150}
+              />
           </main>
+
       </>
   );
 }
